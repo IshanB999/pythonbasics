cb8fc0fff72d935dc9357d51e660fdae5bcda319
Ishan bartaula <ishan1223@gmail.com>
2024-08-09 21:15:46 +0545

    Matchcase statement

--- main.py
+++ main.py
@@ -19,10 +19,28 @@
 
 #If else condition
 
-a=int(input("Enter a number"))
-if (a>0 and a<=10):
-    print("a is a number between 0-10")
-elif(a>10 and a<100):
-    print("a is a number between 10 and 100")
-else:
-    print("a is either zero , negative or greater than 100")
+# a=int(input("Enter a number"))
+# if (a>0 and a<=10):
+#     print("a is a number between 0-10")
+# elif(a>10 and a<100):
+#     print("a is a number between 10 and 100")
+# else:
+#     print("a is either zero , negative or greater than 100")
+
+
+
+
+
+#match case statement
+num=int(input("Enter a number"))
+
+match num:
+    case num if num>0:
+        print("positive number")
+
+    case num if num<0:
+        print("Negative number")
+    case _:
+        print("Zero")
+   
+   
