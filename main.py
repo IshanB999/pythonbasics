5b741982571f6787718de682982b32bd8aea6645
Ishan bartaula <ishan1223@gmail.com>
2024-08-09 20:29:09 +0545

    Assigning same values to multiple variables

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-a=13
-b=4
-print("the value of ",a ,"+",b,"is",a+b)
-print (a-b)
-print(a/b)
-print(a*b)
-print("N new heading is added")
+#Simple mathematical operations
+# a=13
+# b=4
+# print("the value of ",a ,"+",b,"is",a+b)
+# print (a-b)
+# print(a/b)
+# print(a*b)
+
+
+
+
+#assigning same values to multiple variables in same line
+x=y=z=3
+print(x)
+print(y)
+print(z)
+
