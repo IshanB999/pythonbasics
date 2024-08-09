4829ed28abe5615bf40bb3442238360dbb198b35
Ishan bartaula <ishan1223@gmail.com>
2024-08-09 20:53:14 +0545

    If else statement

--- main.py
+++ main.py
@@ -10,8 +10,19 @@
 
 
 #assigning same values to multiple variables in same line
-x=y=z=3
-print(x)
-print(y)
-print(z)
+# x=y=z=3
+# print(x)
+# print(y)
+# print(z)
 
+
+
+#If else condition
+
+a=int(input("Enter a number"))
+if (a>0 and a<=10):
+    print("a is a number between 0-10")
+elif(a>10 and a<100):
+    print("a is a number between 10 and 100")
+else:
+    print("a is either zero , negative or greater than 100")
