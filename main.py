7a82ab0023d9f64d6d7646028096eb304b63458d
Ishan bartaula <ishan1223@gmail.com>
2024-08-15 22:24:02 +0545

    issuperset in set

--- main.py
+++ main.py
@@ -481,13 +481,26 @@
 
 
 #isdisjoint in set
-set1={2,4,6,8,10}
-set2={5,6,7,8,9,11}
-print(set1.isdisjoint(set2))
+# set1={2,4,6,8,10}
+# set2={5,6,7,8,9,11}
+# print(set1.isdisjoint(set2))
+
+# set1={2,4,6,8,10}
+# set2={1,3,5,7,9}
+# print(set1.isdisjoint(set2))
+
+
+#issuperset in set
+set1={1,2,3,4,5,6,7,8,9,10,11,12}
+set2={1,3,5,7,9}
+print(set1.issuperset(set2))
+
 
 set1={2,4,6,8,10}
-set2={1,3,5,7,9}
-print(set1.isdisjoint(set2))
+set2={5,6,7,8,9,11}
+print(set1.issuperset(set2))
+
+
 
 
 
