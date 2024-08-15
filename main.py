db4759af1ff64771e8be7ff77313db8f57077c1f
Ishan bartaula <ishan1223@gmail.com>
2024-08-15 20:58:21 +0545

    Factorial using recurison

--- main.py
+++ main.py
@@ -386,5 +386,15 @@
 
 # print(letter.format(name,university,clgname,age))
 
-    
+
+
+
+#recursion in python
+#function to calculate factorial of anumber using recursion
+def factorial(a):
+    if(a==0 or a==1):
+        return 1
+    else:
+        return a*factorial(a-1)
+print(factorial(5))
 
