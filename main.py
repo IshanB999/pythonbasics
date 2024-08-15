5a3ed340d70eba17d71a650500b237bdf25c2cd0
Ishan bartaula <ishan1223@gmail.com>
2024-08-15 21:41:52 +0545

    Fibonacci sequence using recursion

--- main.py
+++ main.py
@@ -391,10 +391,31 @@
 
 #recursion in python
 #function to calculate factorial of anumber using recursion
-def factorial(a):
-    if(a==0 or a==1):
-        return 1
+# def factorial(a):
+#     if(a==0 or a==1):
+#         return 1
+#     else:
+#         return a*factorial(a-1)
+# print(factorial(5))
+
+
+#fiboncci sequence using recursion
+def fibonacci(n):
+    
+    if n<=1:
+       return n
+        
     else:
-        return a*factorial(a-1)
-print(factorial(5))
+     return(fibonacci(n-1)+fibonacci(n-2))
+
+    
+num_terms=int(input("Enter the number of fibonacci you need :"))
+print(num_terms," fibonacci sequence are ")
+for i in range(num_terms):
+   print(fibonacci(i))
+   
+
+
+
+
 
