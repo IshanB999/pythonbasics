5bb98836bcf96b578b08edbdadaf0f0fa06c5cb5
Ishan bartaula <ishan1223@gmail.com>
2024-11-15 21:46:53 +0545

    Added while loop

--- projects/number_guesser.py
+++ projects/number_guesser.py
@@ -1,13 +1,17 @@
 import random 
 
-top_range=input("Enter the top range: ")
-if top_range.isdigit():
-    top_range=int(top_range)
-    if top_range <=0:
+while True:
+    top_range=input("Enter the top range: ")
+    if top_range.isdigit():
+       top_range=int(top_range)
+       if top_range <=0:
         print("Enter a number greater than 0 for top range ")
-else:
-    print("Please enter a number for top range ")
-    quit()
+        continue
+       if top_range >0:
+        break
+    else:
+        print("Please enter a number for top range ")
+    
 
 rand_num=random.randint(0,top_range)
 guesses=0
