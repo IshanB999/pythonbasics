bf88d3d8e39bb36cb5718d465d398a125dce406b
Ishan bartaula <ishan1223@gmail.com>
2024-11-15 11:26:51 +0545

    Added main.py file

---
+++ basics/add.py
@@ -0,0 +1,13 @@
+import math 
+
+
+def root():
+    a=int(input('Enter the number for calculating squareroot  ' ))
+    sroot=math.sqrt(a)
+    print(sroot)
+
+if __name__=="__main__":
+    root()
+
+
+# hello=print("Hello , Ishan bartaula you are calculating the sqaure root")  
